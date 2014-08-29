de9b4991cd15a73207d58afda70fe993144c6c26
wozio <woziosek@gmail.com>
2014-08-29 08:23:28 +0200

    Adding draft of configuration.py

---
+++ io-control/configuration.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# outputs list: name, type, data
+
+# inputs list: name, type
+
+# events list: name, type, what to run (action or condition), data
+
+# conditions list: name, type, data, what to run (action or condition for two cases: when returning true or false)
+
+# actions list: name, type, data
+
+# services: name, friendly name, list of settings: name, friendly name, type
+
