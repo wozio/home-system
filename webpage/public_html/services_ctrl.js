7926446fcf561f15a45c2343dd724f18d66b1979
wozio <woziosek@gmail.com>
2015-10-23 22:26:54 +0200

    webpage now posts updated setting

--- webpage/public_html/services_ctrl.js
+++ webpage/public_html/services_ctrl.js
@@ -3,5 +3,14 @@ var servicesApp = angular.module('servicesApp', []);
 servicesApp.controller('ServicesCtrl', function ($scope, $http) {
   $http.get('/HSA/execute?name=io-control-dev&msg=get_services').success(function(data) {
     $scope.services = data.services;
+    
+    $scope.updateSetting = function(service, setting, value) {
+      $http.post('/HSA/execute?name=io-control-dev&msg=set_setting_value', {
+          service: service.name,
+          settings: setting.name,
+          value: value
+        }).success(function(data) {
+      });
+    };
   });
 });
