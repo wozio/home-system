b2a7ac42717dd91c36c402b3902c3dfce74b19bd
Ubuntu <ubuntu@ip-172-31-0-88.eu-central-1.compute.internal>
2015-12-21 12:23:11 +0000

    Added interval.

--- webpage/public_html/services_ctrl.js
+++ webpage/public_html/services_ctrl.js
@@ -9,19 +9,25 @@ angular.module('app', [
       new_uri = "ws:";
   }
   new_uri += "//" + loc.host;
-  new_uri += "/access";
+  new_uri += "/access/client/";
   console.log(new_uri);
   var dataStream = $websocket(new_uri);
 
   var services = [];
 
   dataStream.onMessage(function(message) {
+    console.log("Received: " + message.data);
     recvServices = JSON.parse(message.data).services;
     services.splice(0, services.length);
     for (i = 0; i < recvServices.length; i++) { 
       services.push(recvServices[i]);
     }
-    console.log(services);
+    //console.log(services);
+  });
+
+  // firefox WS bug workaround
+  $(window).on('beforeunload', function(){
+    dataStream.close();
   });
 
   var methods = {
@@ -32,6 +38,14 @@ angular.module('app', [
         message: 'get_services',
         expect_reply: true
       }));
+    },
+    get_services: function() {
+      console.log("Sending request");
+      dataStream.send(JSON.stringify({
+        service: "control-server",
+        message: "get_services",
+        expect_reply: true
+      }));
     }
   };
 
@@ -40,4 +54,5 @@ angular.module('app', [
 .controller('ServicesCtrl', function ($scope, MyData) {
   $scope.MyData = MyData;
   MyData.get();
+  window.setInterval(function(){ MyData.get(); }, 5000);
 });
