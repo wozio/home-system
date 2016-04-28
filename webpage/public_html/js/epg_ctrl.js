ea0f88b6f187980cbb8d96d858410352406b2b6b
Piotr Wozniak <woziosek@gmail.com>
2016-04-28 13:55:37 +0200

    started work on epg view

---
+++ webpage/public_html/js/epg_ctrl.js
@@ -0,0 +1,35 @@
+'use strict';
+
+angular.module('app.epg',[
+  'app.data'
+])
+
+.controller('EpgCtrl', [
+  '$scope', '$rootScope', 'DataSrv', '$interval',
+  function ($scope, $rootScope, DataSrv, $interval) {
+    var srv = "tv";
+    var get = function() {
+      $rootScope.error = false;
+      $rootScope.loading = true;
+      DataSrv.send(srv, "get_channels", null, function(result) {
+        if (result.success) {
+          $scope.channels = result.data.channel;
+          $scope.names = result.data.name;
+        } else {
+          console.log("Communication failed with reason: " + result.reason);
+          $rootScope.error = true;
+          $rootScope.errorSlogan = "Communication with Home failed, reason: " + result.reason;
+        }
+        $rootScope.loading = false;
+      });
+    };
+    
+    get();
+    
+    //var interval = $interval(function(){ get(); }, 5000);
+    
+    $scope.$on("$destroy", function(){
+      $interval.cancel(interval);
+    });
+  }
+]);
