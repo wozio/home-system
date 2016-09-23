2a07baa10cadc5d03b2284939059f4010ed50e62
Piotr Wozniak <woziosek@gmail.com>
2016-09-23 11:08:32 +0200

    session seems to be working correctly

--- tv/tv_test/tv_test.cpp
+++ tv/tv_test/tv_test.cpp
@@ -12,10 +12,12 @@ INITIALIZE_EASYLOGGINGPP
 
 #define BUFFER_SIZE 20971520
 
-BOOST_AUTO_TEST_CASE(session_stream_part)
+void test(size_t buf_size, int buf_num)
 {
+  size_t buf_size_total = buf_size * buf_num;
+
   std::vector<char> recv_buf;
-  recv_buf.resize(BUFFER_SIZE);
+  recv_buf.resize(buf_size_total);
   std::atomic<int> recv_len{ 0 };
 
   std::condition_variable cv;
@@ -23,80 +25,51 @@ BOOST_AUTO_TEST_CASE(session_stream_part)
 
   home_system::media::session s(0, [&](int id, void* buf, size_t len, size_t buf_size) {
     BOOST_TEST(id == 0);
-    BOOST_TEST(buf_size == BUFFER_SIZE);
-    BOOST_TEST(recv_len + len <= BUFFER_SIZE);
-    if (recv_len + len <= BUFFER_SIZE)
+    BOOST_TEST(recv_len + len <= buf_size_total);
+    if (recv_len + len <= buf_size_total)
     {
-      char* cbuf = (char*)buf;
-      for (int i = 0; i < len; ++i)
-      {
-        recv_buf[recv_len + i] = cbuf[i];
-      }
+      memcpy(&recv_buf[recv_len], buf, len);
     }
     recv_len += len;
     cv.notify_all();
   });
   std::vector<char> buf;
-  buf.resize(BUFFER_SIZE);
-  for (int i = 0; i < BUFFER_SIZE; i++)
+  buf.resize(buf_size);
+  /*for (int i = 0; i < buf_size; i++)
   {
     buf[i] = i;
+  }*/
+  std::vector<char> sent_buf;
+  for (int i = 0; i < buf_num; ++i)
+  {
+    s.stream_part(&buf[0], buf_size);
+    sent_buf.insert(sent_buf.end(), buf.begin(), buf.end());
   }
-  s.stream_part(&buf[0], BUFFER_SIZE);
 
   using namespace std::chrono_literals;
   std::unique_lock<std::mutex> lk(cv_m);
-  BOOST_TEST((cv.wait_for(lk, 100s, [&]() {return recv_len >= BUFFER_SIZE; }) == true), "Timed out waiting for all data to receive");
+  BOOST_TEST((cv.wait_for(lk, 100s, [&]() {return recv_len >= buf_size_total; }) == true), "Timed out waiting for all data to receive");
 
-  BOOST_TEST(recv_len == BUFFER_SIZE, "received size not correct " << recv_len << "!=" << BUFFER_SIZE);
-  BOOST_TEST(recv_buf == buf, "Buffers not equal");
+  BOOST_TEST(recv_len == buf_size_total);
+  BOOST_TEST(recv_buf == sent_buf, "Buffers not equal");
 }
 
-#define BUFFER_SIZE3 6990506
-#define BUFFER_SIZE3_FULL 6990506 * 4
-
-BOOST_AUTO_TEST_CASE(session_stream_part_4_parts)
+BOOST_AUTO_TEST_CASE(session_1_part_filling_exactly_whole_buffer)
 {
-  std::vector<char> recv_buf;
-  recv_buf.resize(BUFFER_SIZE3_FULL);
-  std::atomic<int> recv_len{ 0 };
+  test(20971520, 1);
+}
 
-  std::condition_variable cv;
-  std::mutex cv_m;
+BOOST_AUTO_TEST_CASE(session_4_parts_filling_more_than_buffer)
+{
+  test(6990506, 4);
+}
 
-  home_system::media::session s(0, [&](int id, void* buf, size_t len, size_t buf_size) {
-    BOOST_TEST(id == 0);
-    BOOST_TEST(recv_len + len <= BUFFER_SIZE3_FULL);
-    if (recv_len + len <= BUFFER_SIZE3_FULL)
-    {
-      char* cbuf = (char*)buf;
-      for (int i = 0; i < len; ++i)
-      {
-        recv_buf[recv_len + i] = cbuf[i];
-      }
-    }
-    recv_len += len;
-    cv.notify_all();
-  });
-  std::vector<char> buf;
-  buf.resize(BUFFER_SIZE3);
-  for (int i = 0; i < BUFFER_SIZE3; i++)
-  {
-    buf[i] = i;
-  }
-  s.stream_part(&buf[0], BUFFER_SIZE3);
-  s.stream_part(&buf[0], BUFFER_SIZE3);
-  s.stream_part(&buf[0], BUFFER_SIZE3);
-  s.stream_part(&buf[0], BUFFER_SIZE3);
+BOOST_AUTO_TEST_CASE(session_11_parts_not_filling_buffer)
+{
+  test(18801, 11);
+}
 
-  std::vector<char> sent_buf;
-  sent_buf.resize(BUFFER_SIZE3_FULL);
-  sent_buf.insert(buf);
-
-  using namespace std::chrono_literals;
-  std::unique_lock<std::mutex> lk(cv_m);
-  BOOST_TEST((cv.wait_for(lk, 100s, [&]() {return recv_len >= BUFFER_SIZE3_FULL; }) == true), "Timed out waiting for all data to receive");
-
-  BOOST_TEST(recv_len == BUFFER_SIZE3_FULL, "received size not correct " << recv_len << "!=" << BUFFER_SIZE3_FULL);
-  BOOST_TEST(recv_buf == buf, "Buffers not equal");
+BOOST_AUTO_TEST_CASE(session_lot_of_small_parts_filling_buffer)
+{
+  test(1880, 20000);
 }
