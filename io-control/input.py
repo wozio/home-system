e481821cf7004186041f6b115b0529365f86dc7f
wozio <woziosek@gmail.com>
2016-11-21 22:45:26 +0100

    work

--- io-control/input.py
+++ io-control/input.py
@@ -2,21 +2,22 @@
 
 import time
 import logging
-import service
-
-state_unknown = 0
-state_ok = 1
+import discovery
 
 class input:
 
-    def __init__(self, input_name, input_service, input_id):
-        self.id = input_id
-        self.service = input_service
-        self.name = input_name
+    state_unknown = 0
+    state_ok = 1
+
+    def __init__(self, name, service, id):
+        self.id = id
+        self.service = service
+        self.name = name
         self.state = state_unknown
         self.value = 0
         self.callbacks = []
         self.history = []
+        discovery.register(self.on_service)
         # first history entry, unknown state, 0 value ignored anyways due to state
         self.history.append((time.time(), state_unknown, 0))
 
@@ -25,15 +26,21 @@ class input:
     def exit(self):
         pass
 
+    def on_service(self, new_service, available):
+        if not available:
+            if new_service == self.service:
+                logging.debug("IO service '%s' is not available for input 's'", new_service, self.name)
+                self.on_state_change(state_unknown, 0)
+
     def get(self):
         return self.state, self.value
 
     def on_state_change(self, state, value):
-        self.history.append((time.time(), state, value))
         if state != self.state:
             logging.debug("'%s' state changed %d->%d", self.name, self.state, state)
             self.state = state
         if self.state == state_ok and value != self.value:
+            self.history.append((time.time(), state, value))
             logging.debug("'%s' value changed %s->%s", self.name, str(self.value), str(value))
             self.value = value
             for c in self.callbacks:
--- io-control/output.py
+++ io-control/output.py
@@ -1,35 +1,22 @@
 #!/usr/bin/env python
 
 import logging
+import input
 import yami
 import yagent
-import discovery
 
-state_unknown = 0
-state_ok = 1
+class output(input):
 
-class output:
-
-    def __init__(self, output_name, output_service, output_id):
-        self.id = output_id
-        self.service = output_service
-        self.name = output_name
-        self.state = state_unknown
-        self.value = 0
+    def __init__(self, name, service, id):
+        super(output, self).__init__(name, service, id)
         self.wanted_value = None
-        self.callbacks = []
-
-        logging.info("Created output '%s' with service=%s and id=%d", self.name, output_service, output_id)
-
-    def get(self):
-        return self.state, self.value
 
     def set(self, value):
         if value != self.wanted_value:
             self.wanted_value = value
             logging.debug("'%s' wanted value set to %s", self.name, str(value))
 
-        if self.wanted_value != self.value:
+        if self.wanted_value != self.value and self.wanted_value != None:
             self.set_value()
 
     def set_value(self):
@@ -39,21 +26,4 @@ class output:
             params["value"] = int(self.wanted_value)
     
             yagent.agent.send_one_way(discovery.get(self.service), self.service,
-                            "set_output_state", params);
-
-    def on_state_change(self, state, value):
-        if state != self.state:
-            logging.debug("'%s' state changed %d->%d", self.name, self.state, state)
-            self.state = state
-        if self.state == state_ok and value != self.value:
-            logging.debug("'%s' value changed %s->%s", self.name, str(self.value), str(value))
-            self.value = value
-            for c in self.callbacks:
-                c()
-            if self.wanted_value and self.wanted_value != self.value:
-                logging.debug("Wanted value of '%s' different from actual, setting to %s", self.name, str(self.wanted_value))
-                self.set_value()
-
-    def subscribe(self, callback):
-        self.callbacks.append(callback)
-        callback()
+                            "set_output_state", params)
