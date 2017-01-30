5b891d61d1b1e24f235f7d3aa66576fc886b5152
wozio <woziosek@gmail.com>
2017-01-30 22:28:26 +0100

    work

---
+++ io-control/io-control.cpp
@@ -0,0 +1,54 @@
+#include "stdafx.h"
+#include "yamicontainer.h"
+#include "discovery.h"
+#include "logger_init.h"
+#include "app.h"
+
+INITIALIZE_EASYLOGGINGPP
+
+using namespace std;
+namespace po = boost::program_options;
+
+home_system::yc_t _yc;
+home_system::discovery_t _discovery;
+
+int main(int argc, char** argv)
+{
+	po::options_description desc("Allowed options");
+	desc.add_options()
+	("help,h", "produce help message")
+	("daemonize,d", "run as daemon")
+	("config-file,c", po::value<string>()->default_value("io-control.conf"), "path to configuration file")
+	;
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc, argv, desc), vm);
+	po::notify(vm);
+
+	home_system::init_log("io-control-server.log", true);
+
+	LOG(INFO) << "Home System IO Control started";
+
+	try
+	{
+		home_system::app app(vm["config-file"].as<std::string>(), vm.count("daemonize"));
+
+		_yc = home_system::yami_container::create();
+		_discovery = home_system::discovery::create();
+	}
+	catch (const exception& e)
+	{
+		LOG(ERROR) << "EXCEPTION: " << e.what();
+	}
+	catch (...)
+	{
+		LOG(ERROR) << "UNKNOWN EXCEPTION";
+	}
+
+	_discovery.reset();
+	_yc.reset();
+
+	LOG(INFO) << "Home System IO Control exit";
+
+    return 0;
+}
+
