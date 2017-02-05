1f1365df911c2475d58c1d712ba9a61c1e07efe2
wozio <woziosek@gmail.com>
2017-02-05 20:16:47 +0100

    small updates

--- io-control/src/ios.cpp
+++ io-control/src/ios.cpp
@@ -9,19 +9,63 @@ namespace home_system
     ios::ios()
     {
         LOG(INFO) << "IOS: Reading configuration";
-        auto conf = CONFIG.get();
+        auto& conf = CONFIG.get();
         // first create known (written in configuration) ioses
         if (conf.HasMember("inputs"))
         {
-            auto a = conf["inputs"];
+            auto& a = conf["inputs"];
             for (auto itr = a.Begin(); itr != a.End(); ++itr)
             {
                 if (itr->IsObject())
                 {
+					std::string name;
+					std::string type;
+					std::string service;
+					long long id;
+
                     if (itr->HasMember("name"))
                     {
-                        LOG(DEBUG) << "IOS: Input found: " << (*itr)["name"];
+						auto& v = (*itr)["name"];
+						if (v.IsString())
+						{
+							name = v.GetString();
+						}                        
                     }
+					if (itr->HasMember("type"))
+					{
+						auto& v = (*itr)["type"];
+						if (v.IsString())
+						{
+							type = v.GetString();
+						}
+					}
+					if (itr->HasMember("service"))
+					{
+						auto& v = (*itr)["service"];
+						if (v.IsString())
+						{
+							service = v.GetString();
+						}
+					}
+					if (itr->HasMember("id"))
+					{
+						auto& v = (*itr)["id"];
+						if (v.IsInt64())
+						{
+							id = v.GetInt64();
+						}
+					}
+					else
+					{
+						continue;
+					}
+					if (name.length() > 0 && type.length() > 0 && service.length() > 0)
+					{
+						if (type == "temp")
+						{
+							LOG(DEBUG) << "IOS: Creating input: " << name;
+						}
+					}
                 }
             }
         }
--- io-control/src/ios.h
+++ io-control/src/ios.h
@@ -15,6 +15,7 @@ public:
     }
     ios();
     ~ios();
+private:
 };
 
 }
